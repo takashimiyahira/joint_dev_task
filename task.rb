81ac0af037cdfbeea72b1f22b9ce8f3bdddb742f
宮平隆 <miyahiratakashi@miyahiratakashinoMacBook-Air.local>
2021-06-28 06:46:21 +0900

    Q6Q7の修正

--- task.rb
+++ task.rb
@@ -48,18 +48,16 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers1.each do |i|
-  numbers2 = i * 10
-  puts numbers2
-  end
+  numbers2 = numbers1.map{|x| x*10}
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  puts array.map!{|x| x.to_i}
-  # 以下は変更しないで下さい
+  array.map!(&:to_i)
+  # 以下は変更しないで下さいruby main.rb
   p array
 end
 
@@ -67,7 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -77,14 +76,21 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.map{|x| x.any?"うに"}
+ if true
+  puts "好物です"
+else
+  puts "まぁまぁ好きです"
+end
 end
 
 def q11
